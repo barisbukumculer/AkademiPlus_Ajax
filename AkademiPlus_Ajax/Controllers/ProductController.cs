05b4c0d22ff37ec19ef40ec5eb9a4be8fd1820b3
barisbukumculer <126320079+barisbukumculer@users.noreply.github.com>
2023-05-23 22:16:33 +0300

    Add project files.

---
+++ AkademiPlus_Ajax/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using AkademiPlus_Ajax.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace AkademiPlus_Ajax.Controllers
+{
+    public class ProductController : Controller
+    {
+        Context context=new Context();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProductList()
+        {
+            var values= context.Products.ToList();
+            var jsonvalues=JsonConvert.SerializeObject(values);
+            return Json(jsonvalues);
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+           context.Products.Add(product);   
+            context.SaveChanges();
+            var values=JsonConvert.SerializeObject(product);
+            return Json(values);
+        }
+        public IActionResult DeleteProduct(int id)
+        {
+            var values = context.Products.Find(id);
+            context.Products.Remove(values);
+            context.SaveChanges();
+            return NoContent();
+        }
+        public IActionResult GetByID(int ProductID)
+        {
+            var values = context.Products.Find(ProductID);
+            var jsonvalues= JsonConvert.SerializeObject(values);
+            return Json(jsonvalues);
+        }
+        public IActionResult UpdateProduct(Product product)
+        {
+            context.Products.Update(product);
+            context.SaveChanges();
+            var values=JsonConvert.SerializeObject(product);
+            return Json(values);
+        }
+    }
+}
---
+++ AkademiPlus_Ajax/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AkademiPlus_Ajax.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-5SF4DGP\\SQLEXPRESS;initial Catalog=AkademiPlusAjaxDb;integrated Security=true");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
